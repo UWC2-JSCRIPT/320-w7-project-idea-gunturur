1dec1793ed651c39f210f6c503167f2973ffdc52
gunturur <ramakanth.gunturu@gmail.com>
2023-03-20 19:07:57 -0700

    Changed from browser router to hashrouter

--- occam-budget/src/App.js
+++ occam-budget/src/App.js
@@ -4,7 +4,9 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import IncomeForm from "./IncomeForm";
 import ExpensesForm from "./ExpensesForm";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+//import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import HomePage from "./HomePage";
 import Visualization from "./Visualization";
 
